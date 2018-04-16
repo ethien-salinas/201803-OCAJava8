dd0ea15e819a7ded87a7ad29c4dbfd408b411a52
Ethien Salinas <ethien.salinas@gmail.com>
2018-04-15 22:26:25 -0500

    [UPDATE] configuration to use JUnit 5

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,13 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
     @Test
-    public void testAppHasAGreeting() {
+    void testAppHasAGreeting() {
         App app = new App();
         assertNotNull("app should have a greeting", app.getGreeting());
         assertEquals("Hello world with Gradle.", app.getGreeting());
