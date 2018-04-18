725a1ea46425c1343988b3f59284e8dc698e7735
Ethien Salinas <ethien.salinas@gmail.com>
2018-04-17 21:57:20 -0500

    [ADD] DisplayName to tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,11 +2,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
     @Test
+    @DisplayName("Greeting tests")
     void testAppHasAGreeting() {
         App app = new App();
         assertNotNull("app should have a greeting", app.getGreeting());
@@ -15,11 +17,12 @@ public class AppTest {
     }
 
     @Test
+    @DisplayName("Math operations tests")
     void testNumbersOperations() {
         int precedence = 25 - 5 * 4 / 2 - 10 + 4;
         int precedenceOverriden = (((25 - 5) * 4) / (2 - 10)) + 4;
-        assertEquals(9, precedence);
-        assertEquals(-6, precedenceOverriden);
+        assertEquals(9, precedence, "the natural order of precedence");
+        assertEquals(-6, precedenceOverriden, "precedence order overridden");
     }
 
 }
