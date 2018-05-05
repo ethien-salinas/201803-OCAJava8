3ccae34afc7d08229ab324cfd3d66979ca68ca88
Ethien Salinas <ethien.salinas@gmail.com>
2018-05-04 22:20:53 -0500

    [ADD] filter and split an array

--- src/main/java/soccer/League.java
+++ src/main/java/soccer/League.java
@@ -40,5 +40,22 @@ public class League {
 
         System.out.println("Goal scored after " + currGame.goals[0].theTime + " mins by "
                 + currGame.goals[0].thePlayer.playerName + " of " + currGame.goals[0].theTeam.teamName);
+
+        for (Player thePlayer : team2.playerArray) {
+            if (thePlayer.playerName.matches(".*Sab.*")) {
+                System.out.println("Found " + thePlayer.playerName);
+                System.out.println("Last name is " + thePlayer.playerName.split(" ")[1]);
+            }
+        }
+
+        StringBuilder familyNameFirst = new StringBuilder();
+        for (Player thePlayer : team1.playerArray) {
+            String[] name = thePlayer.playerName.split(" ");
+            familyNameFirst.append(name[1]);
+            familyNameFirst.append(", ");
+            familyNameFirst.append(name[0]);
+            System.out.println(familyNameFirst);
+            familyNameFirst.delete(0, familyNameFirst.length());
+        }
     }
 }
