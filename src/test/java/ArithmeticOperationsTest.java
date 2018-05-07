0a9f01e380df845c4f7efd5f1d2ecaa1286dc647
Ethien Salinas <ethien.salinas@gmail.com>
2018-05-06 21:48:45 -0500

    [ADD] Cast and promotion test

--- src/test/java/ArithmeticOperationsTest.java
+++ src/test/java/ArithmeticOperationsTest.java
@@ -13,4 +13,21 @@ public class ArithmeticOperationsTest {
         assertThat(precedence).isEqualTo(9);
         assertThat(precedenceOverriden).isEqualTo(-6);
     }
+
+    @Test
+    @DisplayName("Cast and promotion tests")
+    void testCastAndPromotion() {
+        int num1 = 55555;
+        int num2 = 66666;
+        long num3 = num1 * num2;
+        assertThat(num3).isNotEqualTo(3_703_629_630L); // not even possible to express 3_703_629_630 as an int
+        assertThat(num3).isEqualTo(-591_337_666);
+
+        short aShort = 53;
+        short bShort = 47;
+        // before to be assigned to cShort, the + operator produces an int
+        // without cast the result, the operation do not even compile
+        short cShort = (short) (aShort + bShort);
+        assertThat(cShort).isEqualTo(100);
+    }
 }
