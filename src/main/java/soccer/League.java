7431cbc733e1ccd5949c54859f7aa4699a824d42
Ethien Salinas <ethien.salinas@gmail.com>
2018-05-16 07:17:04 -0500

    [ADD] use of constructors to simplify objects build

--- src/main/java/soccer/League.java
+++ src/main/java/soccer/League.java
@@ -9,47 +9,39 @@ public class League {
         Team[] theTeams = theLeague.createTeams();
         Game[] theGames = theLeague.createGames(theTeams);
 
-        Game currGame = theGames[0];
-
-        currGame.playGame();
-
-        System.out.println(currGame.getDescription());
+        for (Game currGame : theGames) {
+            currGame.playGame();
+            System.out.println(currGame.getDescription());
+        }
 
     }
 
     public Team[] createTeams() {
-        Player player1 = new Player();
-        player1.playerName = "George Eliot";
-        Player player2 = new Player();
-        player2.playerName = "Graham Greene";
-        Player player3 = new Player();
-        player3.playerName = "Geoffrey Chaucer";
+        Player player1 = new Player("George Eliot");
+        Player player2 = new Player("Graham Greene");
+        Player player3 = new Player("Geoffrey Chaucer");
         Player[] thePlayers = { player1, player2, player3 };
 
-        Team team1 = new Team();
-        team1.teamName = "The Greens";
-        team1.playerArray = thePlayers;
+        Team team1 = new Team("The Greens", thePlayers);
 
         // Create team2
         Team team2 = new Team();
-        team2.teamName = "The Reds";
-        team2.playerArray = new Player[3];
-        team2.playerArray[0] = new Player();
-        team2.playerArray[0].playerName = "Robert Service";
-        team2.playerArray[1] = new Player();
-        team2.playerArray[1].playerName = "Robbie Burns";
-        team2.playerArray[2] = new Player();
-        team2.playerArray[2].playerName = "Rafael Sabatini";
+        team2.setTeamName("The Reds");
+        team2.setPlayerArray(new Player[3]);
+        team2.getPlayerArray()[0] = new Player("Robert Service");
+        team2.getPlayerArray()[1] = new Player("Robbie Burns");
+        team2.getPlayerArray()[2] = new Player("Rafael Sabatini");
 
         Team[] theTeams = { team1, team2 };
         return theTeams;
     }
 
     public Game[] createGames(Team[] theTeams) {
-        Game theGame = new Game();
-        theGame.homeTeam = theTeams[0];
-        theGame.awayTeam = theTeams[1];
-        Game[] theGames = { theGame };
+        Game theGame = new Game(theTeams[0], theTeams[1]);
+        Game theGame2 = new Game(theTeams[1], theTeams[0]);
+        Game theGame3 = new Game(theTeams[0], theTeams[1]);
+        Game theGame4 = new Game(theTeams[1], theTeams[0]);
+        Game[] theGames = { theGame, theGame2, theGame3, theGame4 };
         return theGames;
     }
 
