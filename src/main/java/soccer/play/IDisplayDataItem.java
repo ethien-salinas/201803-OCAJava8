440da74d47323240f4b4765bb8f2b163c70002bb
Ethien Salinas <ethien.salinas@gmail.com>
2018-06-01 07:50:42 -0500

    [UPDATE] Refactor League

---
+++ src/main/java/soccer/play/IDisplayDataItem.java
@@ -0,0 +1,13 @@
+package soccer.play;
+
+public interface IDisplayDataItem {
+
+    public boolean isDetailAvailable();
+
+    public String getDisplayDetail();
+
+    public int getID();
+
+    public String getDetailType();    // return simple text String or return the actual object?
+
+}
---
+++ src/main/java/soccer/play/Player.java
@@ -0,0 +1,44 @@
+package soccer.play;
+
+public class Player implements Comparable {
+
+    private String playerName;
+    private int goalsScored;
+
+    public void incGoalsScored() {
+        this.goalsScored++;
+    }
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public Player() {
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+
+    public int compareTo(Object thePlayer) {
+
+        if (this.getGoalsScored() < ((Player) thePlayer).getGoalsScored()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
