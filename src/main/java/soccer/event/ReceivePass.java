019bbc44ed53dbd1456bd13448cffc305ec959b6
Ethien Salinas <ethien.salinas@gmail.com>
2018-06-01 07:48:00 -0500

    [ADD] GameEvent

---
+++ src/main/java/soccer/event/ReceivePass.java
@@ -0,0 +1,26 @@
+package soccer.event;
+
+public class ReceivePass extends GameEvent {
+
+    public ReceivePass() {
+        super();
+    }
+
+    public String toString() {
+        return "Receive pass ";
+    }
+
+    public GameEvent[] getNextEvents() {
+        GameEvent theEvent[] = {new Dribble(), new GainPossession(), new Shoot(), new Pass()};
+        return theEvent;
+    }
+
+    public boolean changePlayer() {
+        return true;
+    }
+
+    public boolean changeTeam() {
+        return false;
+    }
+
+}
